c0f15c2668f63a01cbedbd5b71fbfee7e077c315
ShunMatsumoto <mr.bigmr.little@gmail.com>
2019-12-18 16:24:49 +0900

    retake

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -56,7 +56,29 @@ $(function(){
     return html;
   }
 
-  
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.new_message')[0].reset();
+      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      $('.chat-main__message-form--box__submit-btn').prop('disabled', false);
+    })
+    .fail(function() {
+      alert('通信に失敗しました');
+    })
+  })
 
 
     var reloadMessages = function() {
@@ -71,7 +93,6 @@ $(function(){
           dataType: 'json',
           data: {id: last_message_id}          //dataオプションでリクエストに値を含める
         })
-        
         .done(function(messages) {
           var insertHTML = '';          //追加するHTMLの入れ物を作る
           $.each(messages, function(i, message) { 　//配列のmessagesの中身を一つ一つ取り出し、HTMLに変換したものを入れ物に足し合わせる
@@ -82,31 +103,6 @@ $(function(){
           $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
         })
     }
-    
-
-    $('#new_message').on('submit', function(e){
-      e.preventDefault();
-      var formData = new FormData(this);
-      var url = $(this).attr('action')
-      $.ajax({
-        url: url,
-        type: 'POST',
-        data: formData,  
-        dataType: 'json',
-        processData: false,
-        contentType: false
-      })
-      .done(function(data){
-        var html = buildHTML(data);
-        $('.messages').append(html);
-        $('.new_message')[0].reset();
-        $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
-        $('.chat-main__message-form--box__submit-btn').prop('disabled', false);
-      })
-      .fail(function() {
-        alert('通信に失敗しました');
-      })
-    })
   }
   setInterval(reloadMessages, 7000);
 });
